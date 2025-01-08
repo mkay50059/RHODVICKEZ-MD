86bbd55d6876bb58f8fb7273bd66870d5275dc3b
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2025-01-09 01:08:38 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-    pattern: "menu2",
+    pattern: "menu",
     alias: "allmenu",
     desc: "menu the bot",
     category: "menu",
@@ -26,215 +26,215 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃★╰──────────────
 ╰━━━━━━━━━━━━━━━┈⊷
 ╭━━〔 *Download Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• facebook
-┃◈┃• mediafire
-┃◈┃• tiktok
-┃◈┃• twitter
-┃◈┃• Insta
-┃◈┃• apk
-┃◈┃• img
-┃◈┃• tt2
-┃◈┃• git
-┃◈┃• spotify
-┃◈┃• play
-┃◈┃• play2
-┃◈┃• play3
-┃◈┃• audio
-┃◈┃• video
-┃◈┃• video2
-┃◈┃• ytmp3
-┃◈┃• ytmp4
-┃◈┃• song
-┃◈┃• darama
-┃◈┃• gdrive
-┃◈┃• smovie
-┃◈┃• baiscope 
-┃◈┃• ginisilia 
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• facebook
+┃✦┃• mediafire
+┃✦┃• tiktok
+┃✦┃• twitter
+┃✦┃• Insta
+┃✦┃• apk
+┃✦┃• img
+┃✦┃• tt2
+┃✦┃• git
+┃✦┃• spotify
+┃✦┃• play
+┃✦┃• play2
+┃✦┃• play3
+┃✦┃• audio
+┃✦┃• video
+┃✦┃• video2
+┃✦┃• ytmp3
+┃✦┃• ytmp4
+┃✦┃• song
+┃✦┃• darama
+┃✦┃• gdrive
+┃✦┃• smovie
+┃✦┃• baiscope 
+┃✦┃• ginisilia 
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Group Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• grouplink
-┃◈┃• kickall
-┃◈┃• kickall2
-┃◈┃• kickall3
-┃◈┃• add
-┃◈┃• remove
-┃◈┃• kick
-┃◈┃• promote 
-┃◈┃• demote
-┃◈┃• dismiss 
-┃◈┃• revoke
-┃◈┃• setgoodbye
-┃◈┃• setwelcome
-┃◈┃• delete 
-┃◈┃• getpic
-┃◈┃• ginfo
-┃◈┃• delete 
-┃◈┃• disappear on
-┃◈┃• disappear off
-┃◈┃• disappear 7D,24H
-┃◈┃• allreq
-┃◈┃• updategname
-┃◈┃• updategdesc
-┃◈┃• joinrequests
-┃◈┃• senddm
-┃◈┃• nikal
-┃◈┃• mute
-┃◈┃• unmute
-┃◈┃• lockgc
-┃◈┃• unlockgc
-┃◈┃• invite
-┃◈┃• tag
-┃◈┃• hidetag
-┃◈┃• tagall
-┃◈┃• tagadmins
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• grouplink
+┃✦┃• kickall
+┃✦┃• kickall2
+┃✦┃• kickall3
+┃✦┃• add
+┃✦┃• remove
+┃✦┃• kick
+┃✦┃• promote 
+┃✦┃• demote
+┃✦┃• dismiss 
+┃✦┃• revoke
+┃✦┃• setgoodbye
+┃✦┃• setwelcome
+┃✦┃• delete 
+┃✦┃• getpic
+┃✦┃• ginfo
+┃✦┃• delete 
+┃✦┃• disappear on
+┃✦┃• disappear off
+┃✦┃• disappear 7D,24H
+┃✦┃• allreq
+┃✦┃• updategname
+┃✦┃• updategdesc
+┃✦┃• joinrequests
+┃✦┃• senddm
+┃✦┃• nikal
+┃✦┃• mute
+┃✦┃• unmute
+┃✦┃• lockgc
+┃✦┃• unlockgc
+┃✦┃• invite
+┃✦┃• tag
+┃✦┃• hidetag
+┃✦┃• tagall
+┃✦┃• tagadmins
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Owner Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• owner
-┃◈┃• menu
-┃◈┃• menu2
-┃◈┃• vv
-┃◈┃• listcmd
-┃◈┃• allmenu
-┃◈┃• repo
-┃◈┃• block
-┃◈┃• unblock
-┃◈┃• fullpp
-┃◈┃• setpp
-┃◈┃• restart
-┃◈┃• shutdown
-┃◈┃• updatecmd
-┃◈┃• alive
-┃◈┃• ping 
-┃◈┃• gjid
-┃◈┃• jid
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• owner
+┃✦┃• menu
+┃✦┃• menu2
+┃✦┃• vv
+┃✦┃• listcmd
+┃✦┃• allmenu
+┃✦┃• repo
+┃✦┃• block
+┃✦┃• unblock
+┃✦┃• fullpp
+┃✦┃• setpp
+┃✦┃• restart
+┃✦┃• shutdown
+┃✦┃• updatecmd
+┃✦┃• alive
+┃✦┃• ping 
+┃✦┃• gjid
+┃✦┃• jid
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Fun Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• insult
-┃◈┃• hack
-┃◈┃• ship
-┃◈┃• character
-┃◈┃• pickup 
-┃◈┃• joke
-┃◈┃• hrt
-┃◈┃• hpy
-┃◈┃• syd
-┃◈┃• anger
-┃◈┃• shy
-┃◈┃• kiss
-┃◈┃• mon
-┃◈┃• cunfuzed
-┃◈┃• setpp
-┃◈┃• hand
-┃◈┃• nikal
-┃◈┃• hold
-┃◈┃• hug
-┃◈┃• nikal
-┃◈┃• hifi
-┃◈┃• poke
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• insult
+┃✦┃• hack
+┃✦┃• ship
+┃✦┃• character
+┃✦┃• pickup 
+┃✦┃• joke
+┃✦┃• hrt
+┃✦┃• hpy
+┃✦┃• syd
+┃✦┃• anger
+┃✦┃• shy
+┃✦┃• kiss
+┃✦┃• mon
+┃✦┃• cunfuzed
+┃✦┃• setpp
+┃✦┃• hand
+┃✦┃• nikal
+┃✦┃• hold
+┃✦┃• hug
+┃✦┃• nikal
+┃✦┃• hifi
+┃✦┃• poke
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Convert Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• sticker
-┃◈┃• sticker2
-┃◈┃• fancy
-┃◈┃• take
-┃◈┃• tomp3
-┃◈┃• tts
-┃◈┃• trt
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• sticker
+┃✦┃• sticker2
+┃✦┃• fancy
+┃✦┃• take
+┃✦┃• tomp3
+┃✦┃• tts
+┃✦┃• trt
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Ai Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• ai
-┃◈┃• gpt
-┃◈┃• meta
-┃◈┃• blackbox
-┃◈┃• gpt4
-┃◈┃• bing
-┃◈┃• copilot
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• ai
+┃✦┃• gpt
+┃✦┃• meta
+┃✦┃• blackbox
+┃✦┃• gpt4
+┃✦┃• bing
+┃✦┃• copilot
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Main Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• ping
-┃◈┃• ping2
-┃◈┃• speed
-┃◈┃• live 
-┃◈┃• alive
-┃◈┃• runtime
-┃◈┃• uptime 
-┃◈┃• repo
-┃◈┃• owner
-┃◈┃• menu
-┃◈┃• menu2
-┃◈┃• restart
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• ping
+┃✦┃• ping2
+┃✦┃• speed
+┃✦┃• live 
+┃✦┃• alive
+┃✦┃• runtime
+┃✦┃• uptime 
+┃✦┃• repo
+┃✦┃• owner
+┃✦┃• menu
+┃✦┃• menu2
+┃✦┃• restart
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Anime Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• fack
-┃◈┃• dog
-┃◈┃• awoo
-┃◈┃• garl
-┃◈┃• waifu
-┃◈┃• neko
-┃◈┃• megnumin
-┃◈┃• neko
-┃◈┃• maid
-┃◈┃• loli
-┃◈┃• animegirl
-┃◈┃• animegirl
-┃◈┃• animegirl1
-┃◈┃• animegirl2
-┃◈┃• animegirl3
-┃◈┃• animegirl4
-┃◈┃• animegirl5
-┃◈┃• anime1
-┃◈┃• anime1
-┃◈┃• anime2
-┃◈┃• anime3
-┃◈┃• anime4
-┃◈┃• anime5
-┃◈┃• animenews
-┃◈┃• foxgirl
-┃◈┃• naruto
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• fack
+┃✦┃• dog
+┃✦┃• awoo
+┃✦┃• garl
+┃✦┃• waifu
+┃✦┃• neko
+┃✦┃• megnumin
+┃✦┃• neko
+┃✦┃• maid
+┃✦┃• loli
+┃✦┃• animegirl
+┃✦┃• animegirl
+┃✦┃• animegirl1
+┃✦┃• animegirl2
+┃✦┃• animegirl3
+┃✦┃• animegirl4
+┃✦┃• animegirl5
+┃✦┃• anime1
+┃✦┃• anime1
+┃✦┃• anime2
+┃✦┃• anime3
+┃✦┃• anime4
+┃✦┃• anime5
+┃✦┃• animenews
+┃✦┃• foxgirl
+┃✦┃• naruto
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 ╭━━〔 *Other Menu* 〕━━┈⊷
-┃◈╭─────────────·๏
-┃◈┃• fact
-┃◈┃• pair
-┃◈┃• pair2
-┃◈┃• fancy
-┃◈┃• logo <text>
-┃◈┃• define
-┃◈┃• news
-┃◈┃• movie
-┃◈┃• weather
-┃◈┃• srepo
-┃◈┃• insult
-┃◈┃• save
-┃◈┃• wikipedia
-┃◈┃• gpass
-┃◈┃• githubstalk
-┃◈┃• yts
-┃◈┃• ytv
-┃◈└───────────┈⊷
+┃✦╭─────────────·๏
+┃✦┃• fact
+┃✦┃• pair
+┃✦┃• pair2
+┃✦┃• fancy
+┃✦┃• logo <text>
+┃✦┃• define
+┃✦┃• news
+┃✦┃• movie
+┃✦┃• weather
+┃✦┃• srepo
+┃✦┃• insult
+┃✦┃• save
+┃✦┃• wikipedia
+┃✦┃• gpass
+┃✦┃• githubstalk
+┃✦┃• yts
+┃✦┃• ytv
+┃✦└───────────┈⊷
 ╰──────────────┈⊷
 > ${config.DESCRIPTION}`;
 
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.imgur.com/PEZ5QL2.jpeg` },
+                image: { url: `https://i.ibb.co/wJBxKV4/74421a3c5d94ac0a.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
