86fb4569024a7e3daa55575504f17fcd37d4459f
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2025-01-09 01:46:09 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363200367779016@newsletter',
+                    newsletterJid: '120363284845910703@newsletter',
                     newsletterName: 'RHODVICK TECH',
                     serverMessageId: 143
                 }            
