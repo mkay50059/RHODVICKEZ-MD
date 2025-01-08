ec3e20be3ba2cddda493a840e09c21739d69a522
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2025-01-09 00:49:04 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -241,8 +241,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363200367779016@newsletter',
-                        newsletterName: 'SILVA SPARK MD 💖🦄',
+                        newsletterJid: '120363284845910703@newsletter',
+                        newsletterName: '✦RHODVICKEZ MD✦',
                         serverMessageId: 143
                     }
                 }
