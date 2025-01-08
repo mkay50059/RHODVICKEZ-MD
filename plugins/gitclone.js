2ae17e9688af36c56d0a940503b7d0f9208a3311
Rhodvick <150789919+Rhodvick@users.noreply.github.com>
2025-01-09 01:55:01 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -36,7 +36,7 @@ cmd({
     }
     let _0x49a379 = _0xe6be44.headers.get("content-disposition");
     let _0x481150 = _0x49a379 ? _0x49a379.match(/filename=(.*)/)[0x1] : _0x16a926 + ".zip";
-    ✦RHODVICKEZ MD✦ DOWNLOADING REPOSITORY...*\n\n*REPOSITORY:* " + _0x5194b2 + '/' + _0x16a926 + "\n*FILENAME:* " + _0x481150 + "\n\n> *© 𝘚𝘪𝘭𝘷𝘢 𝘚𝘱𝘢𝘳𝘬 𝘔𝘋*");
+    ✦RHODVICKEZ MD✦ DOWNLOADING REPOSITORY...*\n\n*REPOSITORY:* " + _0x5194b2 + '/' + _0x16a926 + "\n*FILENAME:* " + _0x481150 + "\n\n> *✦RHODVICKEZ MD✦*");
     await _0x359a4d.sendMessage(_0x421a7d, {
       'document': {
         'url': _0x936878
